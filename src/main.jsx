e4a977c4527e43a639399640b2707bd97a12319d
Mateus Anderle da Silva <mateus@qxs.local>
2022-09-22 16:56:29 -0300

    Instruction for next steps

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import { Delivery } from './pages/Delivery';
 import { Home } from './pages/Home';
 import { Cart } from './pages/Cart';
 
-
+//Adicionar react-router-dom - Para navegação
+//Otimizar o uso do React-Hook-Form nos input existents
+//Criar todo a regra de negócio e funcionalidades solicitadas.
+//Resolvers e validators - YUP / ZOD
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
